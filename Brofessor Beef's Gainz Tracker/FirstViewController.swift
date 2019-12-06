fa539dd21536a4081888097d2e2793937c5619c4
Michael Martinelli <martinellimichael15@gmail.com>
2019-12-06 11:03:31 -0800

    Created repo and added existing content. Majority of scenes are completed with navigation. No data manipulation yet

---
+++ Brofessor Beef's Gainz Tracker/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Brofessor Beef's Gainz Tracker
+//
+//  Created by Michael Martinelli on 11/12/19.
+//  Copyright © 2019 Michael Martinelli. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ Brofessor Beef's Gainz Tracker/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Brofessor Beef's Gainz Tracker
+//
+//  Created by Michael Martinelli on 11/12/19.
+//  Copyright © 2019 Michael Martinelli. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
